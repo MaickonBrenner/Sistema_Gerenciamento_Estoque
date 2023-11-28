8cda1227ba73ae5dd788de3d13df704f4f4b295b
DaltonLinconl <daltonlinconl19@hotmail.com>
2023-11-28 13:15:13 -0300

    ListaProdutos

--- Sistema/script.js
+++ Sistema/script.js
@@ -20,6 +20,7 @@ function cadastrarProduto() {
     } else {
         alert("O produto foi cadastrado com sucesso!")
         limparFormulario();
+        redirecionarParaListaProdutos()
     }
 
     
@@ -31,4 +32,65 @@ function limparFormulario() {
     document.getElementById("descricao").value = "";
     document.getElementById("preco").value = "";
     document.getElementById("categoria").value = "item0";
+}
+
+function redirecionarParaListaProdutos() {
+    window.location.href = 'ListaProdutos.html'; // Altere para o caminho correto da sua página ListaProdutos
+}
+
+function buscarProdutos() {
+    var searchTerm = document.getElementById("search").value;
+    // Lógica para buscar produtos com base no termo de pesquisa
+    // Atualize a lista de produtos exibidos na página
+    var listaProdutos = getTodosProdutos(); // Substitua por sua lógica real
+    exibirListaProdutos(listaProdutos);
+}
+
+
+function cadastrarNovoProduto() {
+    // Redireciona para a página de cadastro de produtos
+    window.location.href = 'index.html'; // Substitua pelo caminho correto
+}
+
+function exibirListaProdutos(produtos) {
+    var listaProdutosElement = document.getElementById("lista-produtos");
+    listaProdutosElement.innerHTML = ""; // Limpa a lista antes de exibir novamente
+
+    if (produtos.length === 0) {
+        listaProdutosElement.innerHTML = "Nenhum produto encontrado.";
+        return;
+    }
+
+    produtos.forEach(function(produto) {
+        // Crie elementos HTML para exibir cada produto na lista
+        var produtoElement = document.createElement("div");
+        produtoElement.innerHTML = `
+            <p><strong>ID:</strong> ${produto.id}</p>
+            <p><strong>Nome:</strong> ${produto.nome}</p>
+            <p><strong>Descrição:</strong> ${produto.descricao}</p>
+            <p><strong>Preço:</strong> ${produto.preco}</p>
+            <p><strong>Categoria:</strong> ${produto.categoria}</p>
+            <button onclick="atualizarProduto(${produto.id})">Atualizar</button>
+        `;
+
+        listaProdutosElement.appendChild(produtoElement);
+    });
+}
+
+// Função fictícia para simular a lógica de obtenção de todos os produtos
+function getTodosProdutos() {
+    // Supondo que você tenha uma estrutura de dados de produtos
+    // Substitua por sua lógica real
+    return [
+        { id: 1, nome: "Produto 1", descricao: "Descrição do Produto 1", preco: 19.99, categoria: "Categoria 1" },
+        { id: 2, nome: "Produto 2", descricao: "Descrição do Produto 2", preco: 29.99, categoria: "Categoria 2" },
+        // Adicione mais produtos conforme necessário
+    ];
+}
+
+// Função fictícia para simular a lógica de atualização de um produto
+function atualizarProduto(produtoId) {
+    // Implemente a lógica para atualizar o produto com o ID fornecido
+    // Redirecione para a página de atualização do produto
+    window.location.href = 'AtualizarProduto.html?id=' + produtoId; // Substitua pelo caminho correto
 }
