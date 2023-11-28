5e20a5dae9790faf42447dc2d3e1dc147ab823d9
DaltonLinconl <daltonlinconl19@hotmail.com>
2023-11-28 12:54:15 -0300

    atualização no layout

--- Sistema/script.js
+++ Sistema/script.js
@@ -1,10 +1,14 @@
 
 function cadastrarProduto() {
+    
+    event.preventDefault();
 
     var nome = document.getElementById("nome").value;
     var descricao = document.getElementById("descricao").value;
     var preco = document.getElementById("preco").value;
     var categoria = document.getElementById("categoria").value;
+
+    
     
     if (nome.length < 4 || nome.length > 20) {
         alert("Você adicionou mais caracteres do que o permitido em 'nome'")
@@ -12,8 +16,19 @@ function cadastrarProduto() {
         alert("Preço inválido!");
     } else if (categoria == "item0") {
         alert("Por favor, escolha a categoria!")
+        
     } else {
         alert("O produto foi cadastrado com sucesso!")
+        limparFormulario();
     }
 
+    
+
 };
+
+function limparFormulario() {
+    document.getElementById("nome").value = "";
+    document.getElementById("descricao").value = "";
+    document.getElementById("preco").value = "";
+    document.getElementById("categoria").value = "item0";
+}
